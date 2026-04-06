d7592f26c4f095c7f8f620dcabc3498aaaa8110b
Max Van den Eynde <maxvdec@gmail.com>
2026-04-06 11:16:52 +0200

    Initial Commit

---
+++ Fluid Simulation/Fluid_SimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fluid_SimulationApp.swift
+//  Fluid Simulation
+//
+//  Created by Max Van den Eynde on 06/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Fluid_SimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fluid SimulationTests/Fluid_SimulationTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fluid_SimulationTests.swift
+//  Fluid SimulationTests
+//
+//  Created by Max Van den Eynde on 06/04/2026.
+//
+
+import Testing
+@testable import Fluid_Simulation
+
+struct Fluid_SimulationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
